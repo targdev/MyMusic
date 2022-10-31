2e861aead7992d28308d95ded621d972a259648a
Tarcisio Guimarães <79273548+targdev@users.noreply.github.com>
2022-10-31 17:30:41 -0300

    Create a project with Startup and controller configured

---
+++ src/MyMusic.API/Controllers/MyMusicController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyMusic.Infrastructure.DataProviders.Repositories;
+using MyMusic.Application.Models;
+using MyMusic.Application.UseCases;
+using MyMusic.Application.UseCases.Abstratcs;
+
+namespace MyMusic.API.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class MyMusicController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public MyMusicController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(typeof(List<AcquiredMusics>), 400)]
+        [ProducesResponseType(204)]
+        [Route("musics/{name}")]
+        public ActionResult<List<AcquiredMusics>> GetMusics(string name)
+        {
+
+            ISearchMusicsUseCase searchMusicsUseCase = new SearchMusicsUseCase();
+            ICasesValidationUseCase validationUseCase = new CasesValidationUseCase();
+
+            var validationInput = validationUseCase.ValidationSearchMusic(name);
+            var resultSearch = searchMusicsUseCase.SearchingByMusicName(_context, name);
+            var checkListValidation = validationUseCase.ValidationCheckListMusic(resultSearch);
+
+            if (!validationInput) return StatusCode(400);
+            if (!checkListValidation) return StatusCode(204);
+
+            return resultSearch;
+        }
+    }
+}
